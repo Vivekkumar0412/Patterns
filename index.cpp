a5c4b35fc5b8a61da3e88491312108712837a3a6
Vivek Kumar <www.vivekkumar0412@gmail.com>
2023-02-07 10:18:50 +0530

    rectangle star pattern

--- index.cpp
+++ index.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+void recpat(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<" * ";
+        };
+        cout<<endl;
+    }
+};
+
+
 int main(){
-    cout<<"Patterns";
+    cout<<"Patterns"<<endl;
+    int n = 10;
+    // recpat(n);
+    hollowrec(n);
     return 0;
 };
