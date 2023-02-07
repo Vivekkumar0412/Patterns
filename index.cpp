f44ebfeb7879969cd31fcddc555813a0e167f5b7
Vivek Kumar <www.vivekkumar0412@gmail.com>
2023-02-07 10:19:27 +0530

    hollow  rectangle star pattern

--- index.cpp
+++ index.cpp
@@ -9,7 +9,18 @@ void recpat(int n){
     }
 };
 
-
+void hollowrec(int n){
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n; j++){
+            if(i == 1 || i == n || j ==1 || j == n){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            };
+        };
+        cout<<endl;
+    }
+}
 int main(){
     cout<<"Patterns"<<endl;
     int n = 10;
