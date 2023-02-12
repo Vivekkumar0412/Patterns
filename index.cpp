2bcaa6e4ab770e4f1b68a7114024affacdaaa47c
Vivek Kumar <www.vivekkumar0412@gmail.com>
2023-02-12 10:24:24 +0530

    rotated star pattern

--- index.cpp
+++ index.cpp
@@ -20,11 +20,30 @@ void hollowrec(int n){
         };
         cout<<endl;
     }
-}
+};
+
+void rotaterstar(int n){
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<"*";
+        };
+        cout<<endl;
+    };
+
+    for(int i = 1; i<=n; i++){
+        for(int j = i; j<=n; j++){
+            cout<<"*";
+        };
+        cout<<endl;
+    }
+};
+
+
 int main(){
     cout<<"Patterns"<<endl;
     int n = 10;
-    // recpat(n);
+    recpat(n);
     hollowrec(n);
+    rotaterstar(n);
     return 0;
 };
